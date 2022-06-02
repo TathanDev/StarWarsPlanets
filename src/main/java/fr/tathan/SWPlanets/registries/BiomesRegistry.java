9851a3e89e01eb48b7f38f58fb34a18fa192c2f8
TATHAN <costa.tathan@gmail.com>
2022-06-02 14:26:45 +0200

    Add BiomeRegistry

---
+++ src/main/java/fr/tathan/SWPlanets/registries/BiomesRegistry.java
@@ -0,0 +1,10 @@
+package fr.tathan.SWPlanets.registries;
+
+import net.minecraft.resources.ResourceLocation;
+import fr.tathan.SWPlanets.SWPlanets;
+
+public class BiomesRegistry {
+
+    public static final ResourceLocation TATOOINE_DESERT = new ResourceLocation(SWPlanets.MODID, "tatooine_desert");
+
+}
