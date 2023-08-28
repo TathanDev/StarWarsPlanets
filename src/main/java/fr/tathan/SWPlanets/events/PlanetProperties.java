db9b4e955f4b52f4178b10544ed29913d00a45c0
TATHAN <costa.tathan@gmail.com>
2023-08-28 19:06:01 +0200

    Modrinth Upload !

--- src/main/java/fr/tathan/SWPlanets/events/PlanetProperties.java
+++ src/main/java/fr/tathan/SWPlanets/events/PlanetProperties.java
@@ -1,10 +1,14 @@
 package fr.tathan.SWPlanets.events;
 
+import com.google.common.collect.ImmutableSet;
 import fr.tathan.SWPlanets.SWPlanets;
 import fr.tathan.SWPlanets.registries.PlanetsRegistry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.mrscauthd.beyond_earth.events.Methods;
 
 import java.util.Set;
 
@@ -65,15 +69,34 @@ public class PlanetProperties {
 
     );
 
-    /**
+
     @SubscribeEvent
     public static void init(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
+
+            /**
             Methods.worldsWithoutRain = new ImmutableSet.Builder<ResourceKey<Level>>().addAll(Methods.worldsWithoutRain).addAll(worldsWithoutRain).build();
             Methods.spaceWorldsWithoutOxygen = new ImmutableSet.Builder<ResourceKey<Level>>().addAll(Methods.spaceWorldsWithoutOxygen).addAll(spaceWorldsWithoutOxygen).build();
             Methods.spaceWorlds = new ImmutableSet.Builder<ResourceKey<Level>>().addAll(Methods.spaceWorlds).addAll(spaceWorlds).build();
-            Methods.orbitWorlds = new ImmutableSet.Builder<ResourceKey<Level>>().addAll(Methods.orbitWorlds).addAll(orbitWorlds).build();
+
+            Methods.worldsWithoutRain.add(PlanetsRegistry.MANDALORE);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.TATOOINE);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.TATOOINE_ORBIT);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.ENDOR_ORBIT);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.HOT_ORBIT);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.MUSTAFAR);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.MUSTAFAR_ORBIT);
+            Methods.worldsWithoutRain.add(PlanetsRegistry.MANDALORE_ORBIT);
+
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.MANDALORE);
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.TATOOINE_ORBIT);
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.ENDOR_ORBIT);
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.HOT_ORBIT);
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.MUSTAFAR_ORBIT);
+            Methods.spaceWorldsWithoutOxygen.add(PlanetsRegistry.MANDALORE_ORBIT);
+            */
+
         });
     }
-    */
+
 }
